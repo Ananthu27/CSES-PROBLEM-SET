ff7bad362304ddec17a29fc718a191c42e58708f
Ananthu <kma270999@gmail.com>
2020-06-18 02:17:56 +0530

    solved till permutaions

--- permutations.cpp
+++ permutations.cpp
@@ -9,7 +9,48 @@
 #include <algorithm>
 #include <numeric>
 
+bool test(std::vector<int> v)
+{
+    for (int i = 0; i < v.size(); i++)
+        if (v[i] == i + 1)
+            continue;
+        else
+            return false;
+    return true;
+}
+
 int main()
 {
+    std::vector<int> v;
+
+    std::cout << "enter the size/max_value :";
+    int size;
+    std::cin >> size;
+
+    for (int i = 1; i <= size; i++)
+        v.push_back(i);
+
+    int i = 0;
+
+    do
+    {
+        for (i = 1; i < v.size(); i++)
+            if (abs(v[i - 1] - v[i]) == 1)
+                break;
+        if (i == v.size())
+            break;
+    } while (std::next_permutation(begin(v), end(v)));
+
+    if (!test(v))
+
+    {
+        for (auto value : v)
+            std::cout
+                << value << " ";
+        std::cout << std::endl;
+    }
+    else
+        std::cout << "NO SOLUTION" << std::endl;
+
     return 0;
 }
