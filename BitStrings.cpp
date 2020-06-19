4d30d98b78953fd52c8ec02269484b231f093e9f
Ananthu <kma270999@gmail.com>
2020-06-19 17:47:52 +0530

    submited nuber spiral

---
+++ BitStrings.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#define ul unsigned long long
+using namespace std;
+int main()
+{
+    return 0;
+}
--- NumericalSpiral.cpp
+++ NumericalSpiral.cpp
@@ -1,15 +1,25 @@
-// A number spiral is an infinite grid whose upper - left square has number 1.
-// Here are the first five layers of the spiral :
-
-// Your task is to find out the number in row y and column x.
-
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <numeric>
-
+#define ul unsigned long long
+using namespace std;
 int main()
 {
-    std::cout << "stupid question" << std::endl;
+    ul n;
+    cin >> n;
+    vector<vector<ul>> yx = vector<vector<ul>>(n, vector<ul>(2, 0));
+    for (ul i = 0; i < n; i++)
+        cin >> yx[i][0] >> yx[i][1];
+    for (ul i = 0; i < yx.size(); i++)
+    {
+        if ((yx[i][0] % 2 && yx[i][1] % 2 && yx[i][1] >= yx[i][0]) || (!(yx[i][0] % 2) && yx[i][1] % 2 && yx[i][0] < yx[i][1]))
+            cout << (yx[i][1] * yx[i][1]) - yx[i][0] + 1;
+        else if ((yx[i][0] % 2 && yx[i][1] % 2 && yx[i][1] < yx[i][0]) || (yx[i][0] % 2 && !(yx[i][1] % 2) && yx[i][1] < yx[i][0]))
+            cout << ((yx[i][0] - 1) * (yx[i][0] - 1)) + yx[i][1];
+        else if ((!(yx[i][0] % 2) && yx[i][1] % 2 && yx[i][0] >= yx[i][1]) || (yx[i][0] >= yx[i][1] && !(yx[i][0] % 2) && !(yx[i][1] % 2)))
+            cout << yx[i][0] * yx[i][0] - yx[i][1] + 1;
+        else
+            cout << ((yx[i][1] - 1) * (yx[i][1] - 1)) + yx[i][0];
+        cout << endl;
+    }
     return 0;
 }
