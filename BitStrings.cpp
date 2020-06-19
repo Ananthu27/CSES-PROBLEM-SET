abd5d0eb9e907a9edc865108baea5f74a6fbd193
Ananthu <kma270999@gmail.com>
2020-06-19 18:27:57 +0530

    submited bit string

--- BitStrings.cpp
+++ BitStrings.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
-#include <vector>
-#define ul unsigned long long
+#define ul long
 using namespace std;
 int main()
 {
+    ul n, m = 1000000007, a = 1;
+    cin >> n;
+    for (ul i = 0; i < n; i++)
+    {
+        a *= 2;
+        a %= m;
+    }
+    cout << a % m << endl;
     return 0;
 }
